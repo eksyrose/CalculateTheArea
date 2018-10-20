e73abc2e25c857391c5fb59c2da67a72cb54666d
eksyrose <eksyrose@gmail.com>
2018-10-20 19:57:33 +0300

    first commit

---
+++ CalculateLibraryTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+using ClassLibraryCalculateTheArea;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CalculateLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCircles()
+        {
+            int[] radiuses = new int[] { 2, 3, 4 };
+            double[] areas = new double[] { 12.5663706144, 28.2743338823, 50.2654824574 };
+            for (int i = 0; i < radiuses.Length; i++)
+            {
+                double result = General.CalcTheArea(radiuses[i]);
+                Assert.AreEqual(result, areas[i], 0.000001, String.Format("Expected for '{0}'; Actual: {1}",
+                                     result, areas[i]));
+            }
+        }
+
+        [TestMethod]
+        public void TestRectangles()
+        {
+            int [,] values = new int[,] { { 4, 5, 6 }, { 3, 4, 5 }, { 5, 6, 7 } };
+            double[] areas = new double[] { 9.921567416, 6.0, 14.69693845 };
+            for (int i = 0; i < 3; i++)
+            {
+                double result = General.CalcTheArea(values[i, 0], values[i, 1], values[i, 2]);
+                Assert.AreEqual(result, areas[i], 0.000001, String.Format("Expected for {0}; Actual: {1}",
+                                     areas[i], result));
+            }
+        }
+
+        //....test for null
+
+        //.... add wrong data
+    }
+}
